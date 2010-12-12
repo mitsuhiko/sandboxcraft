8bdddaf9960f32ff5282a6b6abe80c3c12be7173
Armin Ronacher <armin.ronacher@active-4.com>
2010-12-12 23:09:20 +0100

    Added octaves for caves

--- src/worldgen.c
+++ src/worldgen.c
@@ -36,8 +36,8 @@ block_is_solid(const sc_worldgen_t *worldgen, float x, float y, float z)
     float noise, density, center_falloff, plateau_falloff;
     
     /* caves */
-    if (powf(sc_perlin_noise3(worldgen->perlin, x * 5.0f, y * 5.0f,
-                              z * 5.0f), 3.0f) < -0.5f)
+    if (powf(sc_perlin_noise3_ex(worldgen->perlin, x * 5.0f, y * 5.0f,
+                                 z * 5.0f, 3), 3.0f) < -0.5f)
         return 0;
 
     /* falloff from the top */
@@ -75,7 +75,7 @@ generate_rock(const sc_worldgen_t *worldgen, sc_world_t *world)
                     (float)y / worldgen->world_size,
                     (float)z / worldgen->world_size
                 ) ? SC_BLOCK_STONE : SC_BLOCK_AIR;
-                sc_world_set_block(world, x, y, z, block);
+                sc_world_set_block_fast(world, x, y, z, block);
             }
 }
 
@@ -84,7 +84,7 @@ erode_down(const sc_worldgen_t *worldgen, sc_world_t *world,
            int x, int y, int z)
 {
     sc_blocktype_t block;
-    int levels_left = 2 + 4 * (sc_perlin_noise3(worldgen->perlin,
+    int levels_left = 1 + 4 * (sc_perlin_noise3(worldgen->perlin,
         x / (float)worldgen->world_size,
         y / (float)worldgen->world_size,
         z / (float)worldgen->world_size
@@ -141,13 +141,15 @@ sc_worldgen_new_world(const sc_worldgen_t *worldgen)
 {
     sc_world_t *world = sc_new_world(worldgen->world_size);
 
-    /* fill the world with one large floating rock (and maybe some small rocks) */
+    /* fill the world with one large floating rock (and maybe some small
+       rocks).  This uses fast block setting and because of that must
+       absolutely happen first. */
     generate_rock(worldgen, world);
 
     /* next step is eroding stone into dirt */
     erode_world(worldgen, world);
 
-    /* grow grass on the high dirt levels */
+    /* grow grass on the high dirt levels when they touch air */
     grow_grass(worldgen, world);
 
     return world;
